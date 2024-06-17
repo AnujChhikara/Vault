dbb0586cee0583fab166b39d7b412911aed5eb74
AnujChhikara <anujchhikara777@gmail.com>
2024-06-17 19:19:30 +0530

    working with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const config = {
+  siteUrl: "https://www.devvault.xyz",
+  generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+  // Other available options..
+};
+
+module.exports = config;
